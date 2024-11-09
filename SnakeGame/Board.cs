eec5e504f41fb7f35b10b8828138a3d0f5984090
Breezilyer <emilpeter22@gmail.com>
2024-11-09 13:57:58 +0100

    Made Board.cs, Game.cs and Player.cs

---
+++ SnakeGame/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Board
+    {
+        private char[,] Grid;
+        private int rows;
+        private int cols;
+
+        public Board(char[,] grid)
+        {
+            this.Grid = grid;
+            this.rows = grid.GetLength(0);
+            this.cols = grid.GetLength(1);
+        }
+
+        public void Draw()
+        {
+            for (int x = 0; x < this.rows; x++)
+                for (int y = 0; y < this.cols; y++)
+                {
+                    char element = Grid[x,y];
+                    if (element == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else if (element == 'S')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+
+
+                    Console.SetCursorPosition(y, x);
+                    Console.Write(Grid[x, y]);
+                }
+        }
+    }
+}
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace SnakeGame
+﻿using static System.Console;
+namespace SnakeGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game game = new Game();
+            game.Start();
         }
     }
 }
